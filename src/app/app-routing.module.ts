0911d3d7eaca06a17f8f2b1d1bd74f0a4fa76a57
lqhuyhust <lqhuy.hust@gmail.com>
2021-03-31 23:45:30 +0700

    embed bootstrap + add header + add footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowseComponent } from './browse/browse.component';
+import { SoftwareComponent } from './software/software.component';
+import { HelpComponent } from './help/help.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowseComponent } from './browse/browse.component';
+import { SoftwareComponent } from './software/software.component';
+import { HelpComponent } from './help/help.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowseComponent,
+    SoftwareComponent,
+    HelpComponent,
+    DeveloperComponent,
+    TutorialComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
